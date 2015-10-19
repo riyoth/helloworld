0d8afa5e6e6025abd79dc1476fa91ddb8ddb2daf
Pier-Olivier Clement <riyoth@gmail.com>
2015-10-19 00:16:27 -0400

    Add app creation on dokku server

--- fabfile.py
+++ fabfile.py
@@ -3,17 +3,18 @@ from fabric.api import *
 
 
 def create_app(branch):
-    run(''.join(('dokku apps:create ',branch,'-app')))
-    run(''.join(('dokku mariadb:create ',branch,'-db')))
-    run(''.join(('dokku mariadb:link ',branch,'-db ',branch,'-app')))
+     print('do nothing')
+#    run(''.join(('dokku apps:create ',branch,'-app')))
+#    run(''.join(('dokku mariadb:create ',branch,'-db')))
+#    run(''.join(('dokku mariadb:link ',branch,'-db ',branch,'-app')))
     
 
 def push_to_dokku(branch):
     local(''.join(("git remote add test  dokku@dokku.riyoth.info:",branch)))
-    local(''.join(("git push test ",branch)))
+    local("git push test HEAD:master")
 
 @hosts('root@dokku.riyoth.info')
 @task
 def deploy(branch):
-#    create_app(branch)
+    create_app(branch)
     push_to_dokku(branch)
